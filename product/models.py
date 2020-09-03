a51ff0ae3ac5443ed05af89c22007314a0fa2359
igorsilva3 <igorbr2018@gmail.com>
2020-09-03 16:05:00 -0300

    Models files

--- product/models.py
+++ product/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    """Model definition for Categorie."""
+    
+    name = models.CharField(max_length=60, blank=False)
+    created_date = models.DateField(auto_now_add = True)
+
+    class Meta:
+        """Meta definition for Categorie."""
+
+        verbose_name = 'Categorie'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Unicode representation of Categorie."""
+        return self.name
+
+
+class Product(models.Model):
+    """Model definition for Product."""
+
+    name = models.CharField(max_length=60, blank=False)
+    value = models.FloatField()
+    description = models.TextField(blank=True)
+    categorie =  models.CharField(max_length=60, blank=False)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Product."""
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        """Unicode representation of Product."""
+        return self.name
