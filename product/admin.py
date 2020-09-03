c2316bdf35a298d6c5ac14f90b71a4c02be8ff83
igorsilva3 <igorbr2018@gmail.com>
2020-09-03 16:04:12 -0300

    Admin site file

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categorie, Product
 
 # Register your models here.
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_date',)
+    ordering = ['name']
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'categorie', 'created_date', 'updated_date')
+    ordering = ['name']
+
+admin.site.register(Categorie, CategorieAdmin)
+admin.site.register(Product, ProductAdmin)
