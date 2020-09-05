5e44506dad9688301136091c8d2f45a937b1afdc
igorsilva3 <igorbr2018@gmail.com>
2020-09-04 21:39:14 -0300

    Added function static to urlpatterns

--- product_manager/urls.py
+++ product_manager/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('core.urls', namespace='core')),
     path('product/', include('product.urls', namespace='product')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, 
+           document_root=settings.MEDIA_ROOT)
