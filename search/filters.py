3fbcbc174747196eda19948be8edab749e354bfa
igorsilva3 <igorbr2018@gmail.com>
2020-09-11 03:19:47 -0300

    Added new filter

--- search/filters.py
+++ search/filters.py
@@ -4,6 +4,8 @@ from product.models import Product
 
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    brand = django_filters.CharFilter(field_name='brand', lookup_expr='icontains')
+    
     class Meta:
         model = Product
         fields = ['categorie']
